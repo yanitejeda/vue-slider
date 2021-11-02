24fa5a9abaeb5e95348cbdf7148e5a6b5aaff69a
yanitejeda <tejedasp@gmail.com>
2021-11-02 09:18:31 +0100

    impostato scorrimento immagine principale

--- slide.js
+++ slide.js
@@ -1,22 +1,56 @@
-const foto = [
-    {   img : 'img/01.jpg',
-        titolo: 'Svezia',
-        testo : ' del testo a caso',
+Vue.config.devtools = true
+
+
+const slide = new Vue ({
+    el: '#box',
+    data: {
+         elenco : [
+            {   picture : 'img/01.jpg',
+                titolo: 'Svezia',
+                testo : ' del testo a caso',
+            },
+            {   picture : 'img/02.jpg',
+                titolo: 'Svezia',
+                testo : ' del testo a caso',
+            },
+            {   picture : 'img/03.jpg',
+                titolo: 'Svezia',
+                testo : ' del testo a caso',
+            },
+            {   picture : 'img/04.jpg',
+                titolo: 'Svezia',
+                testo : ' del testo a caso',
+            },
+            {   picture : 'img/05.jpg',
+                titolo: 'Svezia',
+                testo : ' del testo a caso',
+            },
+        ],
+        currentActivePicture: 0
     },
-    {   img : 'img/02.jpg',
-        titolo: 'Svezia',
-        testo : ' del testo a caso',
-    },
-    {   img : 'img/02.jpg',
-        titolo: 'Svezia',
-        testo : ' del testo a caso',
-    },
-    {   img : 'img/03.jpg',
-        titolo: 'Svezia',
-        testo : ' del testo a caso',
-    },
-    {   img : 'img/04.jpg',
-        titolo: 'Svezia',
-        testo : ' del testo a caso',
-    },
-]
+
+
+    methods:{
+        arrowUp(){
+           // this.currentActivePicture --
+            
+          let newCurretPicuture = this.currentActivePicture-1
+          if ( newCurretPicuture < 0){
+              newCurretPicuture = this.elenco.length -1;    
+          }
+          this.currentActivePicture = newCurretPicuture
+        },
+
+        arrowDown(){
+           // this.currentActivePicture ++
+            
+          let newCurretPicuture = this.currentActivePicture + 1
+          if ( newCurretPicuture > this.elenco.length - 1 ){
+              newCurretPicuture = 0;    
+          }
+          this.currentActivePicture = newCurretPicuture
+        }
+
+        
+    }
+})
